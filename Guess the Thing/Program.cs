a1350513435cb170dc0451702e9ab9e3681461d8
blockmaster52010 <88914940+blockmaster52010@users.noreply.github.com>
2024-01-19 19:44:53 -0500

    Add files via upload

---
+++ Guess the Thing/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program {
+    public static void Main(string[] args)
+    {
+
+        bool winner = false;
+        Console.Write("Pick An Animal: ");
+        string animal = Console.ReadLine();
+
+        while (winner == false) {
+
+            Console.Write("Ask A Question: ");
+            string choice = Console.ReadLine();
+            Console.WriteLine(choice);
+
+            Console.WriteLine("Is It True: ");
+            string awnser = Console.ReadLine(); 
+
+            Console.Write("If you thing you know the awnser enter 'y' else 'n': ");
+            string isyes = Console.ReadLine();
+            if (isyes == "y") {
+                Console.Write("What is the Animal: ");
+                string what = Console.ReadLine();
+                Console.WriteLine("Is That The Awnser y/n: ");
+                string istrue = Console.ReadLine();
+                if (istrue == "y") {
+                    winner = true;
+                }
+            }
+        }
+    }
+}
